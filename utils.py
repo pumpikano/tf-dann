621692151fd43317de0e48adea3fe57b01a40414
Clayton Mellina <clayton@yahoo-inc.com>
2016-06-17 20:48:48 +0000

    fix MNIST experiment notebook; add tsne viz

--- utils.py
+++ utils.py
@@ -66,3 +66,22 @@ def imshow_grid(images, shape=[2, 8]):
         grid[i].imshow(images[i])  # The AxesGrid object work as a list of axes.
 
     plt.show()
+
+
+def plot_embedding(X, y, d, title=None):
+    """Plot an embedding X with the class label y colored by the domain d."""
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+
+    # Plot colors numbers
+    plt.figure(figsize=(10,10))
+    ax = plt.subplot(111)
+    for i in range(X.shape[0]):
+        # plot colored number
+        plt.text(X[i, 0], X[i, 1], str(y[i]),
+                 color=plt.cm.bwr(d[i] / 1.),
+                 fontdict={'weight': 'bold', 'size': 9})
+
+    plt.xticks([]), plt.yticks([])
+    if title is not None:
+        plt.title(title)
